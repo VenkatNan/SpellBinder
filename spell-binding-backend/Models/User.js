ab04740651ef8dc1695c71b98d28a459bb4205fb
venkatnan <nanditavenkat@gmail.com>
2021-09-17 21:45:57 -0400

    models

--- spell-binding-backend/Models/User.js
+++ spell-binding-backend/Models/User.js
@@ -1,11 +1,22 @@
 const mongoose = require('../db/connection')
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    splLvl: Number,
-    class: String,
-    spells: [String],
-    img: String,
+    name: {
+        type : String,
+        default : 'John'
+    },
+    splLvl: {
+        type : Number,
+        default : 3
+    },
+    class: {
+        type : String,
+        default : 'wizard'
+    },
+    img: {
+        type : String,
+        default : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjrYUuMqVB-EtbwLCrRA87Z19XJmED91rCHw&usqp=CAU'
+    },
 
 
 })
